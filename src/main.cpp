860bb9b5132ee004567f19dba6f45265254b31e1
unknown <thomaskuwaikwan@gmail.com>
2024-10-01 23:14:54 +0800

    updated main.cpp

--- src/main.cpp
+++ src/main.cpp
@@ -1,13 +1,30 @@
 #include <Arduino.h>
-#define photoSignalPin 18
+#define photoSignalPin 4  
 
+bool bootState = false;               //set default state of ESP32 to be off
 
 void setup() {
-pinMode(photoSignalPin, INPUT);
+Serial.begin(9600);                   //set serial monitor communication speed
+pinMode(photoSignalPin, INPUT);       //set photoresistor signal pin to be input 
 
 
 }
 
 void loop() {
+  int light = analogRead(photoSignalPin);       //store the signal pin into light variable
+  int lightLevel = map(light, 0,4095,0,15);     //map the analog values into 16 values (lightlevel is 15 when light is 4095)
+  Serial.println(light);                        //print the light value onto serial monitor
   
+  //check if ESP32 needs to be turned on 
+  if (lightLevel > 11 && !bootState)
+  {
+    Serial.println("Turning on...");
+    bootState = true;
+  }
+  else if (lightLevel <= 11 && bootState)
+  {
+    Serial.println("Turning off...");
+    bootState = false;
+  }
+  delay(1000);
 }
